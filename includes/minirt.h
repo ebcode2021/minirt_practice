6e8223d799a833063571487377eb0ccb0d4866a4
Eunbi Son <eunbison@code.local>
2023-01-29 17:45:39 +0900

    test file

---
+++ includes/minirt.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eunbison <eunbison@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/29 17:16:55 by eunbison          #+#    #+#             */
+/*   Updated: 2023/01/29 17:44:15 by eunbison         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+/* includes*/
+#include "const.h"
+
+/* library */
+#include "libft.h"
+#include "mlx.h"
+
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
