94fbf4b5656843ac8fb0600eb7aa15e520b9c88f
ebcode2021 <ebcode2021@gmail.com>
2023-02-08 22:53:17 +0900

    add::color function

--- include/color.h
+++ include/color.h
@@ -2,6 +2,7 @@
 # define COLOR_H
 
 #include "vec3.h"
+#include "rtweekend.h"
 #include <iostream>
 
 void	write_color(std::ostream &out, color pixel_color, int samples_per_pixel)
@@ -17,7 +18,7 @@ void	write_color(std::ostream &out, color pixel_color, int samples_per_pixel)
 	b *= scale;
 
 	// Write the translated [0, 255] value of each color component.
-	out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+	out << static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << ' '
 		<< static_cast<int>(255.999 * clamp(g, 0.0, 0.999)) << ' '
 		<< static_cast<int>(255.999 * clamp(b, 0.0, 0.999)) << '\n';
 }
